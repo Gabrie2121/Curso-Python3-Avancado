c93c15cc20c0829c3f6db21f043b10a10f3ed496
Gabriel Miranda <63580608+Gabrie2121@users.noreply.github.com>
2021-01-19 20:45:45 -0300

    Aula String padrão finalizada

---
+++ Logica de Python/strings.py
@@ -0,0 +1,9 @@
+#string é um dado primitivo tambem, afinal tudo aqui é objeto
+#str - string
+#string é a mesma coisa do normal entre '###'
+
+print('tudo dentro de aspas e string(str) e tudo padrão')
+# o \\ ignora os '' dentro do string
+print('esse e meu \'texto\'(str)')
+# \n é quebra de linha padrão
+print('esse e meu\n \'texto\'(str)')
