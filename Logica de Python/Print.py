6bb05c5675c95405ed0ec26b85833d8b1bd84fb9
Gabriel Miranda <63580608+Gabrie2121@users.noreply.github.com>
2021-01-19 20:33:00 -0300

    Finalizado aula sobre print

---
+++ Logica de Python/Print.py
@@ -0,0 +1,14 @@
+#exibir algo na tela
+print('hello world')
+print(123456)
+#argumento nomeado sep='any' que da replace espaço entre argumento
+#argumento nomeado end='any' que da replace na quebra de linha
+#print('Gabriel', 'Miranda', 16, 'anos')
+print('Gabriel', 'Miranda', sep='-',end='\n# # # # # # # # #\n')
+print('Gabriel', 'Miranda', sep='-')
+#python é case sensitive
+
+#desafio criar cpf
+
+print(824, 176, '070', sep='.',end='-')
+print(18)
