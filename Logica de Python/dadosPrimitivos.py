02e9a4a00d4f6d3ff96bb678c35469c1ea292b07
Gabriel Miranda <63580608+Gabrie2121@users.noreply.github.com>
2021-01-19 21:27:13 -0300

    Finalizado a aula de Dados basicos primitivos

---
+++ Logica de Python/dadosPrimitivos.py
@@ -0,0 +1,12 @@
+#Tipos de dados:
+# string(str) "assim"
+# inteiro(int) 0 10 20 30 -10 -20 -30
+# real(float) 10.50 1.4 2.5
+# boolean(bool) true/false
+
+
+#funcao type retorna o tipo do valor
+print('Gabriel ',type('Gabriel'),sep=' ### ')
+print('10 ',type(10),sep=' ### ')
+print('10.5 ',type(10.5),sep=' ### ')
+print('10 = 10 ',type(10==10),sep=' ### ')
