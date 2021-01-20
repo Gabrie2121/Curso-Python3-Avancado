5f2abeefe6ab3de1c2634b6cff779911d068c187
Gabriel Miranda <63580608+Gabrie2121@users.noreply.github.com>
2021-01-19 21:55:33 -0300

    Finalizado aula de operações aritmeticas

---
+++ Logica de Python/opAritmeticos.py
@@ -0,0 +1,22 @@
+#operadores aritmeticos
+#  + Adicao
+#  - Subtracao
+#  * Multiplicacao
+#  / Divisao
+#  // Divisão inteira (o resultado é arredondado)
+#  ** exponenciação
+#  % Resto da divisão
+#  () mudar precedencia
+# da para converter os dados com ex: str(23)
+
+print('Adicao + ',10+10)
+#não da pra concatenar int junto com string
+print('Multiplicação * ',10*10)
+#se vc fizer uma mult com uma int e string, ela vai fazer repetição
+#print('Multiplicação * ',10*'a')
+print('Subtração - ', 10-5)
+print('Divisão / ', 10/2)
+print('Divisão inteira // ', 11//2) # inteiro
+print('exponenciação ** ', 10**2) # elevado
+print('Resto da divisão %', 11%2) # modulo
+print('sempre feito primeiro as entre parenteses',(5+2)*10)
