9d3b514c026bc93ca9bad71fe86c7e833780d6a7
Gabriel Miranda <63580608+Gabrie2121@users.noreply.github.com>
2021-01-21 10:21:06 -0300

    Finalizado aula de variaveis

---
+++ Logica de Python/variaveis.py
@@ -0,0 +1,14 @@
+# sobre variaveis
+# inicio sempre letras, tem numeros, separa só com _, letras minusculas
+nome = 'Gabriel'
+idade = 20
+altura = 1.80
+e_maior = idade> 18
+peso = 107
+imc = peso/(altura*altura)
+print('Nome: ', nome)
+print('Idade: ', idade)
+print('Altura: ', altura)
+print('E maior? ', e_maior)
+#Desafio gerar imc
+print(nome, 'tem', idade,'anos de idade e seu imc e', imc)
