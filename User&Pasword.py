12c4261b62cef3fea6ac7ec97faed5ed49edd58e
Gabriel Miranda <63580608+Gabrie2121@users.noreply.github.com>
2021-01-21 14:18:32 -0300

    Finalizado simulador de autenticação de usuarios

---
+++ User&Pasword.py
@@ -0,0 +1,12 @@
+# Autenticador de usuario e senha basico para aprendizado
+usuario = input('Nome de usuario')
+senha = input('Senha de usuario')
+
+usuario_bd = 'Gabriel'
+senha_bd = '123456'
+
+
+if usuario_br == usuario and senha_bd == senha:
+    print('Você está logado no sistema')
+else:
+    print('Usuario ou senha incorretos')
