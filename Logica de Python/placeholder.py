3cdeaa1c96a26250e4bdb9e4e3b947c9da4e11f4
Gabriel Miranda <63580608+Gabrie2121@users.noreply.github.com>
2021-01-22 13:53:04 -0300

    Finalizado aula de placeholder com pass

---
+++ Logica de Python/placeholder.py
@@ -0,0 +1,10 @@
+# Pass e ellipsis como placeholders
+
+valor = True
+
+# pass ou ellipsis caso queira que fique vazio, caso contrario da erro
+if valor:
+    # pode usar ... tambem, 3 pontos vale o mesmo do pass
+    pass
+else:
+    print('tchau')
